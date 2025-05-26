d4ad217f327b97b1645f9d85990e337c519f0c88
rifat-two-eight <rifat28.dev@gmail.com>
2025-05-26 07:42:46 +0600

    career

---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layouts/Root";
+import Home from "../Pages/Home/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import router from "./Router/Router";
+import { RouterProvider } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
