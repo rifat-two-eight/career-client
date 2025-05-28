55226debea635b4e02d9739139cffd8ece120a76
rifat-two-eight <rifat28.dev@gmail.com>
2025-05-28 17:47:08 +0600

    link

--- src/Pages/JobDetails/JobApply.jsx
+++ src/Pages/JobDetails/JobApply.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { useParams } from "react-router";
 
 const JobApply = () => {
+  const { id: jobId } = useParams();
   return (
-    <div>
-      <h2>Apply here</h2>
+    <div className="my-10">
+      <h2 className="text-center mb-5 text-3xl">Apply here</h2>
+      <form className="flex flex-col justify-self-center">
+        <label className="label">Linkedin Link</label>
+        <input type="url" className="input" placeholder="linkedin link" />
+
+        <label className="label">Github Link</label>
+        <input type="url" className="input" placeholder="github link" />
+
+        <label className="label">Resume Link</label>
+        <input type="url" className="input" placeholder="resume link" />
+
+        <input className="btn" type="submit" value="submit" />
+      </form>
     </div>
   );
 };
